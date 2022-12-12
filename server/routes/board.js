21c791115c2c7d65ecc85f7898ee13335659a1bc
@acmahaja <acmahaja@gmail.com>
2022-12-12 17:01:24 +0200

    Added Get request for board

--- server/routes/board.js
+++ server/routes/board.js
@@ -3,15 +3,15 @@ const BoardRouter = express.Router();
 
 const User = require("../model/User");
 const Board = require("../model/Board");
+const Task = require("../model/Task");
 const { verifyAccessToken } = require("../utils/auth");
 const { parseColumns } = require("../utils/stringTools");
-
 
 const dotenv = require("dotenv");
 const { model, Error } = require("mongoose");
 const { saveColumns } = require("../controller/column");
 const { verifyBoard } = require("../controller/board");
-
+const Column = require("../model/Column");
 
 dotenv.config();
 
@@ -25,9 +25,30 @@ BoardRouter.put("/:id", verifyAccessToken, (req, res) => {
   res.send(`editing ${id}`);
 });
 
-BoardRouter.get("/:id", verifyAccessToken, (req, res) => {
-  const { id } = req.params;
-  res.send(`getting ${id}`);
+BoardRouter.get("/:id", verifyAccessToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    let boardObject;
+    const board = await Board.findById(id);
+    boardObject = { name: board };
+
+    let columns = await Column.find({ board: board._id });
+
+    let columnObject = [];
+
+    columns.forEach((column) => {
+      const tasks = Task.find({ column: column._id });
+      columnObject.push({ id: column._id, name: column.name });
+    });
+
+
+    res.send({ status: "ok", ...boardObject, columns: columnObject });
+  } catch (error) {
+    res.send({
+      status: "error",
+      message: `Something broke getting your board`,
+    });
+  }
 });
 
 BoardRouter.post("/", verifyAccessToken, async (req, res) => {
@@ -44,23 +65,22 @@ BoardRouter.post("/", verifyAccessToken, async (req, res) => {
       throw new Error("User doesn't exist");
     }
 
-    let {name, columns} = req.body;
+    let { name, columns } = req.body;
 
-    if (await verifyBoard(name, user._id)>0) {
-      throw new Error('Board Exist')
+    if ((await verifyBoard(name, user._id)) > 0) {
+      throw new Error("Board Exist");
     }
 
-    const newBoard = new Board({ user: user._id,  name: name});    
+    const newBoard = new Board({ user: user._id, name: name });
 
-    
-    if (columns) {      
+    if (columns) {
       columns = parseColumns(columns);
-      columns = saveColumns(columns, newBoard._id)
+      columns = saveColumns(columns, newBoard._id);
     }
 
-    newBoard.save()
-    columns.forEach(async column => column.save())
-    
+    newBoard.save();
+    columns.forEach(async (column) => column.save());
+
     res.send({ status: "ok", board_id: newBoard._id });
   } catch (error) {
     res.send({ status: "error", message: `Error Creating board ${error}` });
